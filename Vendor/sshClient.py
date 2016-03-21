37ff453a727d3b4a7d2d80e03cadcce94415e928
targuan <targuan@gmail.com>
2016-03-21 17:19:19 +0100

    Add tftp server to the daemon

--- Vendor/sshClient.py
+++ Vendor/sshClient.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 import mysql.connector
-from Queue import Queue
+import Queue
 from threading import Thread
 import signal, os, socket
 import time, sys
 import argparse
 import netmiko
 import re
+from tftp import *
 import logging
 from isc_dhcp_leases.iscdhcpleases import Lease, IscDhcpLeases
 
+
+from client import ClientIOSXE
+
 def stop_handler(signum,frame):
   global run
+  logger.info('Shutting down')
   run = False
 
-signal.signal(signal.SIGINT,stop_handler)
 
 def parse_args():
   parser = argparse.ArgumentParser(description="",fromfile_prefix_chars='@')
@@ -24,179 +28,113 @@ def parse_args():
   parser.add_argument('--db-database', required=True,help="Database host")
   parser.add_argument('--sw-user',required=True,help="Equipements user")
   parser.add_argument('--sw-pass',required=True,help="Equipements pass")
-  parser.add_argument('--workers',default=5,help="Equipements pass")
+  parser.add_argument('--workers',default=5,help="Equipements pass",type=int)
   parser.add_argument('--binary',required=True,help="Equipements pass")
   parser.add_argument('--binary-md5',required=True,help="Equipements pass")
   parser.add_argument('--version',required=True,help="Equipements pass")
   parser.add_argument('--ftp-server',required=True,help="Equipements pass")
   parser.add_argument('--tftp-server',required=True,help="Equipements pass")
   parser.add_argument('--leases-file',required=True,help="Equipements pass")
+  parser.add_argument('--debug',help="Debug level",type=int,choices=[0,1,2],default=0)
+  parser.add_argument('--http-root',required=True)
   args = parser.parse_args()
+  
   if not os.path.isfile(args.leases_file):
     sys.exit(1)
   args.leases=IscDhcpLeases(args.leases_file)
+  
+  levels=[logging.WARNING,logging.INFO,logging.DEBUG]
+  logger.setLevel(levels[args.debug])
+  
   return args
 
-def ping(ip,timeout=2):
-  try:
-    s = socket.create_connection((ip, 22),timeout)
-    s.close()
-    return True
-  except:
-    pass
-  return False
+def updateStatus(conn,cursor,status,id):
+  cursor.execute("update equipements set status=%s where id=%s",(status,id))
+  conn.commit()
 
-def checkMd5(net_connect,filename,md5):
-  buf = net_connect.send_command('verify /md5 %s'%filename)
-  if 'Error' in buf:
-    net_connect.clear_buffer()
-    return False
-  print buf
-  while "verify" not in buf:
-    time.sleep(1)
-    buf = net_connect.clear_buffer()
-    print buf
-  m=re.search('verify.*= ([0-9a-f]+)',buf)
-  md5c = m.group(1)
-  if md5 != md5c:
-    return False
-  return True
-
-def checkVersion(net_connect,args):
-  buf = net_connect.send_command('show ver')
-  if args.version in buf:
-    return True
-  return False
-
-def checkBinary(net_connect,args):
-  buf = net_connect.send_command('dir')
-  if ' ' + args.binary +"\n" in buf:
-    return True
-  return False
-
-def download(net_connect,args):
-  net_connect.send_command('copy ftp://anonymous@%s/%s flash:'%(args.ftp_server,args.binary))
-  while '!' in buf:
-    time.sleep(1)
-    buf = net_connect.clear_buffer()
-    print buf
-  if not checkMd5(net_connect,args.binary,args.binary_md5):
-    return False
-  return True
-
-def upgrade(net_connect,args):
-  fbuf = u''
-  buf = net_connect.send_command('soft install file flash:%s'%args.binary)
-  print buf
-  fbuf += buf
-  while 'reload' not in fbuf:
-    time.sleep(1)
-    buf = net_connect.clear_buffer()
-    if buf != None:
-      print buf
-      fbuf += buf
-  if "Do you want to proceed with reload" in fbuf:
-    buf = net_connect.send_command('yes')
-    if buf != None:
-      fbuf += buf
-  if 'System configuration has been modified' in buf:
-    buf = net_connect.send_command('no')
-    if buf != None:
-      fbuf += buf
-  print fbuf
-  time.sleep(10)
-  
-def copyConfig(net_connect,hostname,args):
-  net_connect.send_command('switch %s priority 15'%number)
-  net_connect.send_command('y')
-  buf = net_connect.send_command('copy tftp://%s/fullconfig/%s-confg start'%(args.tftp_server,hostname))
-  if 'OK' in buf:
-    return True
-  return False
-
-def provision(net_connect,number):
-  if number == 1:
-    net_connect.send_command('switch %s priority 15'%number)
-  elif number == 2:
-    net_connect.send_command('switch %s priority 14'%number)
-  else:
-    net_connect.send_command('switch %s priority 1'%number)
-  net_connect.send_command('y')
-  buf = net_connect.send_command('show sw')
-  m = re.search('\*(\d+)',buf)
-  if m:
-    id = m.group(1)
-    if id!=number:
-      buf = net_connect.send_command("sw %s ren %s"%(id,number))
-      buf = net_connect.send_command("y")
+def push(queue,equipement,args):
+  if equipement['mac'] in args.leases.get_current():
+    logger.debug("Pushing back %s"%equipement['hostname'])
+    queue.put(equipement)
 
 def testEquipement(queue,args):
   myconn = mysql.connector.connect(host=args.db_host,user=args.db_user,password=args.db_pass,database=args.db_user)
   mycursor = myconn.cursor()
   while run:
-    equipement = queue.get()
-    device = {
-      'device_type': 'cisco_ios',
-      'ip': equipement['ip'],
-      'username': args.sw_user,
-      'password': args.sw_pass,
-      'verbose':False,
-    }
     try:
-      if ping(equipement['ip']):
-        logging.info("%s ping ok"%equipement['hostname'])
-        mycursor.execute("update equipements set status=%s where id=%s",(1,equipement['id']))
-        myconn.commit()
-        net_connect = netmiko.ConnectHandler(**device)
-        logging.info("%s connected"%equipement['hostname'])
-        mycursor.execute("update equipements set status=%s where id=%s",(2,equipement['id']))
-        myconn.commit()
-        if not checkVersion(net_connect,args):
-          logging.info("%s check version ko"%equipement['hostname'])
-          if not checkBinary(net_connect,args):
-            logging.info("%s check binary ko, downloading"%equipement['hostname'])
-            mycursor.execute("update equipements set status=%s where id=%s",(5,equipement['id']))
-            myconn.commit()
-            download(net_connect,args)
+      equipement = queue.get(True,1)
+      conn=ClientIOSXE(equipement['ip'],args.sw_user,args.sw_pass)
+      logger.debug('%s Running'%equipement['hostname'])
+      if not conn.ping():
+        logger.debug('%s Ping KO'%equipement['hostname'])
+        push(queue,equipement,args)
+        updateStatus(myconn,mycursor,0,equipement['id'])
+        continue
+      
+      logger.debug('%s Ping OK'%equipement['hostname'])
+      updateStatus(myconn,mycursor,1,equipement['id'])
+      
+      try:
+        conn.connect()
+      except:
+        logger.debug('%s Connection KO'%equipement['hostname'])
+        push(queue,equipement,args)
+        continue
+      
+      logger.info("%s Connection OK"%equipement['hostname'])
+      updateStatus(myconn,mycursor,2,equipement['id'])
+      
+      if not conn.checkVersion(args.version):
+        logger.info("%s version KO"%equipement['hostname'])
+        
+        if not conn.fileExists(args.binary):
+          logger.info("%s Binary file not found"%equipement['hostname'])
+          updateStatus(myconn,mycursor,2,equipement['id'])
+          if not conn.download('ftp://%s/%s'%(args.ftp_server,args.binary)):
+            logger.debug('%s Binary download failed'%equipement['hostname'])
+            push(queue,equipement,args)
+            conn.disconnect()
+            continue
           else:
-            logging.info("%s check binary ok"%equipement['hostname'])
-            mycursor.execute("update equipements set status=%s where id=%s",(4,equipement['id']))
-            myconn.commit()
-            if not checkMd5(net_connect,args.binary,args.binary_md5):
-              logging.info("%s check md5 KO"%equipement['hostname'])
-            else:
-              logging.info("%s check md5 ok, upgrading"%equipement['hostname'])
-              mycursor.execute("update equipements set status=%s where id=%s",(6,equipement['id']))
-              myconn.commit()
-              upgrade(net_connect,args)
+            logger.debug('%s Binary download OK'%equipement['hostname'])
+        
+        logger.info("%s Binary found"%equipement['hostname'])
+        updateStatus(myconn,mycursor,4,equipement['id'])
+        if not conn.md5sum(args.binary,args.binary_md5):
+          logger.info("%s Bad md5 for binary"%equipement['hostname'])
+          push(queue,equipement,args)
+          conn.disconnect()
+          continue
         else:
-          logging.info("%s check version ok"%equipement['hostname'])
-          mycursor.execute("update equipements set status=%s where id=%s",(3,equipement['id']))
-          myconn.commit()
-          m=re.search('^slave(\d+)',equipement['template'])
-          if m:
-            id=m.group(1)
-            provision(net_connect,id)
-            logging.info("%s provision OK"%equipement['hostname'])
-            mycursor.execute("update equipements set status=%s where id=%s",(9,equipement['id']))
-          else:
-            provision(net_connect,1)
-            if not copyConfig(net_connect,equipement['hostname'],args):
-              logging.info("%s copy KO"%equipement['hostname'])
-              mycursor.execute("update equipements set status=%s where id=%s",(8,equipement['id']))
-            else:
-              logging.info("%s copy OK"%equipement['hostname'])
-              mycursor.execute("update equipements set status=%s where id=%s",(9,equipement['id']))
-          myconn.commit()
-        net_connect.disconnect()
+          logger.info("%s md5 OK"%equipement['hostname'])
+          updateStatus(myconn,mycursor,6,equipement['id'])
+          conn.upgrade(args.binary)
       else:
-        logging.info("%s ping ko"%equipement['hostname'])
-        mycursor.execute("update equipements set status=%s where id=%s",(0,equipement['id']))
-    except Exception as e:
-      print e
-      pass
-    myconn.commit()
+        logger.info("%s version OK"%equipement['hostname'])
+        updateStatus(myconn,mycursor,3,equipement['id'])
+        m=re.search('^slave(\d+)',equipement['template'])
+        if m:
+          id=m.group(1)
+          conn.provision(id)
+          logger.info("%s provision OK"%equipement['hostname'])
+          updateStatus(myconn,mycursor,9,equipement['id'])
+          conn.disconnect()
+        else:
+          conn.provision(1)
+          if not conn.copyConfig(equipement['hostname'],args.tftp_server):
+            logger.info("%s copy KO"%equipement['hostname'])
+            updateStatus(myconn,mycursor,8,equipement['id'])
+            push(queue,equipement,args)
+            conn.disconnect()
+          else:
+            logger.info("%s copy OK"%equipement['hostname'])
+            updateStatus(myconn,mycursor,9,equipement['id'])
+            conn.disconnect()
+            continue
+    except Queue.Empty:
+      continue
     queue.task_done()
+  myconn.close()
 
 
 def loadList():
@@ -210,55 +148,63 @@ def loadList():
   myconn.disconnect()
   return equipements
 
-def clearQueue(queue):
-  while not queue.empty():
-    queue.get()
-    queue.task_done()
 
-def fillQueue(queue,args):
-  while run:
-    #logging.info('Fill queue')
-    onlineEquipements=getOnlineEquipements(args)
-    for equipement in onlineEquipements:
-      queue.put(equipement)
-    queue.join()
-    time.sleep(1)
 
-def getOnlineEquipements(args):
-  equipements=loadList()
-  leases=args.leases.get_current()
-  online=[]
-  for mac in leases:
-    eq = [e for e in equipements if e['mac']==mac]
-    if len(eq)>0:
-      eq[0]['ip'] = leases[mac].ip
-      online.append(eq[0])
-  return online
+if __name__ == '__main__':
+  logger = logging.getLogger('autoinstall.main')
+  logger.setLevel(logging.DEBUG)
+  ch = logging.StreamHandler()
+  ch.setLevel(logging.DEBUG)
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(filename)s %(lineno)d - %(thread)d - %(levelname)s - %(message)s')
+  ch.setFormatter(formatter)
+  logger.addHandler(ch)
+  logger.propagate = False
+  logger.info('Starting')
   
-
-logging.basicConfig(level=logging.INFO)
-args = parse_args()
-run = True
-equipementList = Queue()
-try:
-  myconn = mysql.connector.connect(host=args.db_host,user=args.db_user,password=args.db_pass,database=args.db_user)
-  mycursor = myconn.cursor()
-except Exception as e:
-  print "Can't connect to database",e
-  sys.exit(1)
-
-myconn.disconnect()
-for i in xrange(args.workers):
-  t = Thread(target=testEquipement,args=(equipementList,args,))
-  t.daemon = True
-  t.start()
-
-t = Thread(target=fillQueue,args=(equipementList,args,))
-t.daemon = True
-t.start()
-
-while run:
-  t.join(1)
-  if not t.isAlive():
-    break
+  args = parse_args()
+  run = True
+  equipementList = Queue.Queue()
+  
+  logger.debug('Installing SIGINT handler')
+  signal.signal(signal.SIGINT,stop_handler)
+  
+  logger.debug('Testing database connection')
+  try:
+    myconn = mysql.connector.connect(host=args.db_host,user=args.db_user,password=args.db_pass,database=args.db_user)
+    mycursor = myconn.cursor()
+  except Exception as e:
+    logger.error("Failed to connect to database: %s"%str(e))
+    sys.exit(1)
+  
+  logger.debug('Starting %d workers'%args.workers)
+  for i in xrange(args.workers):
+    t = Thread(target=testEquipement,args=(equipementList,args,))
+    t.start()
+  
+  logger.debug('Starting tftp server')
+  server=TFTPServer(root='/srv/tftp',leases=args.leases,httproot=args.http_root)
+  server.start()
+  
+  logger.debug('Running')
+  leasesList=set()
+  while run:
+    leases=args.leases.get_current()
+    #logger.debug('Found %d active leases'%len(leases))
+    currentLeasesList=set([(mac,leases[mac].ip) for mac in leases])
+    newLeases=currentLeasesList-leasesList
+    leasesList=currentLeasesList
+    #logger.debug('%d New leases'%len(newLeases))
+    
+    if len(newLeases)>0:
+      equipements=loadList()
+      for lease in newLeases:
+        eq=[e for e in equipements if e['mac']==lease[0]]
+        if len(eq)>0:
+          eq[0]['ip']=lease[1]
+          logger.debug('Add %s to process list'%str(eq[0]))
+          equipementList.put(eq[0])
+        else:
+          logger.debug('Lease for mac %s is not in the database or in finished state'%lease[0])
+    
+    time.sleep(1)
 
---
+++ Vendor/tftp.py
@@ -0,0 +1,77 @@
+import tftpy
+import logging
+import threading
+import isc_dhcp_leases.iscdhcpleases
+import cStringIO
+import urllib2
+
+logger = logging.getLogger('autoinstall.tftp')
+logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(filename)s %(lineno)d - %(thread)d - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+
+
+
+
+logging.getLogger('tftpy').setLevel(500)
+
+class TFTPServer:
+  def __init__(self,address="0.0.0.0",port=69,root="/dev/null",leases=None,httproot=''):
+    self.address = address
+    self.port = port
+    self.root = root
+    self.running = False
+    self.leases = leases
+    self.httproot = httproot
+  
+  def _get(self,filename):
+    file = ''
+    logger.info('Serving %s'%filename)
+    if filename == 'network-confg':
+      leases=self.leases.get_current()
+      for mac in leases:
+        file += "ip host boot %s\n"%(leases[mac].ip)
+    elif filename == 'boot-confg':
+      try:
+        fp = urllib2.urlopen('%s/parameters/get/boottemplate'%self.httproot)
+        file = fp.read()
+        fp.close()
+      except:
+        logger.error("Can't get boottemplate from the webserver")
+        pass
+    elif '-confg' in filename:
+      name = filename[:-6]
+      logger.info("Getting %s from the webserver"%(name))
+      try:
+        logger.debug('%s/equipements/getByHostname/%s'%(self.httproot,name))
+        fp = urllib2.urlopen('%s/equipements/getByHostname/%s'%(self.httproot,name))
+        file = fp.read()
+        fp.close()
+      except:
+        logger.error("Can't get the configuration %s from the webserver"%name)
+        pass
+    return cStringIO.StringIO(file)
+  
+  def _run(self):
+    try:
+      self.server = tftpy.TftpServer(self.root,self._get)
+      self.running = True
+      self.server.listen(self.address, self.port)
+      self.running = False
+    except Exception as e:
+      logger.error("Can't start TFTP server %s",e)
+      self.running = False
+  
+  def start(self):
+    logger.info("Starting TFTP server %s:%s"%(self.address,self.port))
+    t=threading.Thread(None, self._run )
+    t.daemon=True
+    t.start()
+  
+  def stop(self):
+    self.server.stop()
